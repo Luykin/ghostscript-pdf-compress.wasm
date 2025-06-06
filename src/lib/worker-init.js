adc46cd6bc116480740318b2bcc65af71c1eea17
Luykin <530366891@qq.com>
2025-06-06 13:45:04 +0800

    Move gs-worker files to public directory

--- src/lib/worker-init.js
+++ src/lib/worker-init.js
@@ -1,6 +1,6 @@
 export async function _GSPS2PDF(dataStruct) {
   const worker = new Worker(
-    new URL('./background-worker.js', import.meta.url),
+    '/background-worker.js',
     {type: 'module'}
   );
   
